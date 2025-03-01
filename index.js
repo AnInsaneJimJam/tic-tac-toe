57ea638ad07200daf03002ed10ab969cf0ffe20a
12anand34 <anand47bansal@gmail.com>
2025-03-01 06:36:05 +0530

    Fixed getSymbol logic and added Display Turn

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
-let turn = 1; // 1->X and 2->O
+let turn = 2; // 1->X and 2->O
 
+const displayTurn = document.getElementById("displayTurn")
 
-function getSymbol(turn){
-    return turn = 1 ? "X":"O" 
+function getSymbol(turn) {
+    return turn == 1 ? "X" : "O";
 }
 
-function markSymbol(button,turn){
+function markSymbol(button, turn) {
     const symbol = getSymbol(turn);
     button.innerHTML = symbol
 }
+
+function DisplayTurn(turn) {
+    displayTurn.innerHTML = (`TURN : PLAYER ${turn}`);
+}
+
+DisplayTurn(turn)
