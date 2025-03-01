90ff16b42a67f7d252bb4fd403ea12271ed1a354
12anand34 <anand47bansal@gmail.com>
2025-03-01 20:04:49 +0530

    X and O array for winner calc

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-let turn = 2; // 1->X and 2->O
+let turn = 1; // 1->X and 2->O
 const winConditions = [
-    [1,2,3],
-    [4,5,6],
-    [7,8,9],
-    [1,4,7],
-    [2,5,8],
-    [3,6,9],
-    [1,5,9],
-    [3,5,7]
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [1, 4, 7],
+    [2, 5, 8],
+    [3, 6, 9],
+    [1, 5, 9],
+    [3, 5, 7]
 ]
+const X = [];
+const O = [];
 
 const displayTurn = document.getElementById("displayTurn")
 
@@ -19,6 +21,10 @@ function getSymbol(Turn) {
 function markSymbol(button) {
     const symbol = getSymbol(turn);
     button.innerHTML = symbol;
+    DisplayTurn(turn)
+    const idx = button.id
+    symbol == "X" ? X.push(idx) : O.push(idx)
+
     nextTurn()
     button.removeAttribute("onClick")
 }
@@ -27,8 +33,10 @@ function DisplayTurn(turn) {
     displayTurn.innerHTML = (`TURN : PLAYER ${turn}`);
 }
 
-DisplayTurn(turn)
-
 function nextTurn() {
-    turn = (turn == 1 )? 2 : 1
+    turn = (turn == 1) ? 2 : 1
 }
+function goingOn() {
+}
+
+function isWinner() { }
